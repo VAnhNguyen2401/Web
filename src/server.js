b937aaf20b36870135875eab05b7776ba07d17ed
VAnhNguyen2401 <anh.nv235651@sis.hust.edu.vn>
2025-04-07 22:28:14 +0700

    ShowData

--- src/server.js
+++ src/server.js
@@ -2,17 +2,19 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/vieweEngine";
 import initWebRoute from "./route/web";
+import connectDB from "./config/connectDB";
 require('dotenv').config();
 
 
 
 let app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 viewEngine(app);
 initWebRoute(app);
+connectDB();
 
-let port = process.env.PORT ||6969;
+let port = process.env.PORT || 6969;
 //Port == undefined => port = 6969
 
 app.listen(port, () => {
